83945603c0efa70a151db7bbeca22c04c7e399ca
Koksik229 <ikoks.zz@gmail.com>
2025-06-11 13:00:14 +0500

    Avtopush: fixy i zavisimosti [2025-06-11]

--- PUSH.bat
+++ PUSH.bat
@@ -2,11 +2,14 @@
 chcp 1251 > nul
 cd /d D:\Bloody
 
+:: Получаем дату и время в МСК (UTC+3) из локального UTC+5
+for /f %%i in ('powershell -command "(Get-Date).AddHours(-2).ToString('yyyy-MM-dd HH:mm:ss')"' ) do set dt=%%i
+
 echo Dobavlyaem izmeneniya...
 git add .
 
-echo Commitim...
-git commit -m "Avtopush: fixy i zavisimosti"
+echo Commitim s datoy: %dt%
+git commit -m "Avtopush: fixy i zavisimosti [%dt%]"
 
 echo Otpravlyaem na GitHub...
 git push origin master
