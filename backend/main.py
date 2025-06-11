b480c8e9f03085f55b8563c2fe316f800c60ddcc
Koksik229 <ikoks.zz@gmail.com>
2025-06-11 16:06:44 +0500

    Avtopush: fixy i zavisimosti [2025-06-11_14-06-44]

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from routes.auth import router as auth_router
 from routes.profile import router as profile_router
 from config import SECRET_KEY
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import Response
 import os
 from dotenv import load_dotenv
 
@@ -11,19 +12,32 @@ load_dotenv()
 
 app = FastAPI()
 
-origins = os.getenv("BACKEND_CORS_ORIGINS", "").split(",")
+origins = [
+    "http://localhost:5173",
+    "https://bloody-2-front.onrender.com"
+]
 
+# 👇 Логирование Origin + принудительная установка заголовков CORS
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    origin = request.headers.get("origin")
+    print(">>> ORIGIN:", origin)  # Лог в Render
+
+    if origin in origins:
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+
+    return response
+
+# CORS Middleware (дублируем для preflight-OPTIONS)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",  # для локальной разработки
-        "https://bloody-2-front.onrender.com"  # для боевого фронта
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY, max_age=900)
 
